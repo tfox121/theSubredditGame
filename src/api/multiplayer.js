67c864c556f095b2cd95fe61e74ca6cd8b8a2ad6
Tom Fox <tfox2@hotmail.co.uk>
2020-04-01 00:52:36 +0100

    Adjust API routing selection again - reverting to old manual method.

--- src/api/multiplayer.js
+++ src/api/multiplayer.js
@@ -1,21 +1,12 @@
 import axios from 'axios';
 
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config();
-}
 export const source = axios.CancelToken.source();
 
-const baseURL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8000/games'
-    : 'https://subreddit-game-api.herokuapp.com/games';
+// const baseURL = 'http://localhost:8000/games'
+
+const baseURL = 'https://subreddit-game-api.herokuapp.com/games';
 
 export const axiosDefault = axios.create({
   baseURL,
   cancelToken: source.token
 });
-
-// export const axiosDefault = axios.create({
-//   baseURL: 'https://subreddit-game-api.herokuapp.com/games',
-//   cancelToken: source.token
-// });
