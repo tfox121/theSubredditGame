83511dd29e1ba6d20c7187b0cf33475ff77519dc
Tom Fox <tfox2@hotmail.co.uk>
2020-04-02 04:39:12 +0100

    Fix websocket URL.

--- src/api/websocket.js
+++ src/api/websocket.js
@@ -1,5 +1,5 @@
 // const webSocket = new WebSocket('ws://localhost:8000');
 
-const webSocket = new WebSocket('ws://subreddit-game-api.herokuapp.com');
+const webSocket = new WebSocket('wss://subreddit-game-api.herokuapp.com');
 
 export default webSocket;
