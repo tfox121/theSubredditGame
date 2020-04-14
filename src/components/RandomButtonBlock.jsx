08ba03feb63373b357f4b5a00ba9a93f69022dd2
Tom Fox <tfox2@hotmail.co.uk>
2020-04-14 03:04:11 +0100

    Adjust main header line height.

--- src/components/RandomButtonBlock.jsx
+++ src/components/RandomButtonBlock.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { reduxForm } from 'redux-form';
 
+import './RandomButtonBlock.css';
+
 const RandomButtonBlock = ({ handleSubmit, onSubmit }) => {
   const nsfwSubmitHandler = (formValues) => {
     onSubmit({ ...formValues, nsfw: 0 });
@@ -10,7 +12,7 @@ const RandomButtonBlock = ({ handleSubmit, onSubmit }) => {
     <div className="ui basic segment random-button-block">
       <form onSubmit={handleSubmit(nsfwSubmitHandler)} className="ui form">
         <div className="field">
-          <button className="ui animated button" tabIndex="0" type="submit">
+          <button className="ui animated button generate-sub" tabIndex="0" type="submit">
             <div className="visible content">generate subreddit</div>
             <div className="hidden content">woooooooo!</div>
           </button>
