25d99b820fb10e04c7c095db493e4ac11eefc81d
tfox121 <tfox2@hotmail.co.uk>
2020-04-16 00:36:03 +0100

    Update production API baseURL

--- src/api/guesses.js
+++ src/api/guesses.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:8000/guess'
-  : 'https://subreddit-game-api.herokuapp.com/guess';
+  : 'https://thesubredditgameapi.herokuapp.com/guess';
 
 export default axios.create({ baseURL });
--- src/api/multiplayer.js
+++ src/api/multiplayer.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:8000/games'
-  : 'https://subreddit-game-api.herokuapp.com/games';
+  : 'https://thesubredditgameapi.herokuapp.com/games';
 
 export default axios.create({ baseURL });
