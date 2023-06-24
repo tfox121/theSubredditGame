2f15309a12af52b2b07247361261f8a2cb4e51b8
tfox121 <thomas.fox@compassdigital.io>
2023-06-24 00:40:01 -0400

    chore: revert unnecessary changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { BreakpointProvider } from 'react-socks';
 
@@ -49,7 +49,7 @@ const App = (props) => {
 
   return (
     <BreakpointProvider>
-      <BrowserRouter history={history} basename={process.env.REACT_APP_PUBLIC_URL}>
+      <Router history={history}>
         <div className="ui container">
           <div className="ui app basic segment">
             <Header />
@@ -80,7 +80,7 @@ const App = (props) => {
           </div>
         </div>
         {webSocketErrorRender()}
-      </BrowserRouter>
+      </Router>
     </BreakpointProvider>
   );
 };
